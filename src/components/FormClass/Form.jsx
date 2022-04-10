8bfd3dd9f911ce32bfcbcb31fde8ecbc5bb12ba5
JPustik <96074985+JPustik@users.noreply.github.com>
2022-04-10 20:52:29 +0300

    Update Form.jsx

--- src/components/FormClass/Form.jsx
+++ src/components/FormClass/Form.jsx
@@ -3,7 +3,7 @@ import { Input } from './Input';
 import { Button } from './Button';
 
 export class Form extends Component {
-    state = {
+  state = {
     name: 'click',
     value: '',
     messages: [],
@@ -24,7 +24,7 @@ export class Form extends Component {
       {this.state.messages.map(message =>
         <li>{message}</li>
       )}
-           </ul>}
+    </ul>}
 
 <Input change={this.handleChange} value={this.state.value} />
 <Button name={this.state.name} click={this.handleClick} />
